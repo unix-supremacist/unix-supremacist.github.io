112c8ad946bd063e3041abb16828faca74eb9801
UnixSupremacist <100294596+unix-supremacist@users.noreply.github.com>
2023-05-18 15:58:37 -0500

    move over page changes and make buildscript generic

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ FILES2="posts/*.html"
 FILES4=$(ls posts | grep .ms| sed s/.ms//)
 TMP0="tmp"
 TMP1="../tmp"
-FOOTER=$(sed -z 's/\n/\\n/g' < tpl/footer.tpl)
-NAV0=$(sed -z 's/\n/\\n/g' < tpl/nav.tpl)
-NAV1=$(sed -z 's/\n/\\n/g' < tpl/nav1.tpl)
-POSTHEAD=$(sed -z 's/\n/\\n/g' < tpl/posthead.tpl)
-HEAD0=$(sed -z 's/\n/\\n/g' < tpl/head.tpl)
-HEAD1=$(sed -z 's/\n/\\n/g' < tpl/head1.tpl)
+OS=$(uname)
+[ "$OS" = "Linux" ] && SED="sed"
+[ "$OS" = "OpenBSD" ] && SED="gsed"
+FOOTER=$($SED -z 's/\n/\\n/g' < tpl/footer.tpl)
+NAV0=$($SED -z 's/\n/\\n/g' < tpl/nav.tpl)
+NAV1=$($SED -z 's/\n/\\n/g' < tpl/nav1.tpl)
+POSTHEAD=$($SED -z 's/\n/\\n/g' < tpl/posthead.tpl)
+HEAD0=$($SED -z 's/\n/\\n/g' < tpl/head.tpl)
+HEAD1=$($SED -z 's/\n/\\n/g' < tpl/head1.tpl)
 
 for f in $FILES4
 do
@@ -18,26 +21,26 @@ done
 
 for FILE in $FILES0
 do
-	sed -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP0
+	$SED -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP0
 	mv -f $TMP0 $FILE
-	sed -z "s;<header>.*<img;$NAV0;g" $FILE > $TMP0
+	$SED -z "s;<header>.*<img;$NAV0;g" $FILE > $TMP0
 	mv -f $TMP0 $FILE
-	sed -z "s;<head>.*</head>;$HEAD0;g" $FILE > $TMP0
+	$SED -z "s;<head>.*</head>;$HEAD0;g" $FILE > $TMP0
 	mv -f $TMP0 $FILE
 done
 
 for FILE in $FILES1
 do
-	sed -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP1
+	$SED -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
-	sed -z "s;<header>.*<img;$NAV1;g" $FILE > $TMP1
+	$SED -z "s;<header>.*<img;$NAV1;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
-	sed -z "s;<head>.*</head>;$HEAD1;g" $FILE > $TMP1
+	$SED -z "s;<head>.*</head>;$HEAD1;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
 done
 
 for FILE in $FILES2
 do
-	sed -z "s;<head>.*</style>;$POSTHEAD;g" $FILE > $TMP1
+	$SED -z "s;<head>.*</style>;$POSTHEAD;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
 done
--- tpl/footer.tpl
+++ tpl/footer.tpl
@@ -1,4 +1,5 @@
 <footer>
+			<a href="mailto:unixsupremacist@tfwno.gf">Email Me</a>
 			<a href="https://github.com/unix-supremacist/unix-supremacist.github.io">Source</a>
 			<a href="https://matrix.to/#/#the-federation:matrix.thisisjoes.site">Matrix</a>
 			<a href="https://unix-supermacist.github.io/rss.xml">Rss</a>
